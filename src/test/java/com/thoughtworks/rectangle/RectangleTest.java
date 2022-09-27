0742b657784d49562036f627e6d3345f95a57c70
Ritika Saxena <ritika.saxena@thoughtworks.com>
2022-09-27 11:44:29 +0530

    Add. area

---
+++ src/test/java/com/thoughtworks/rectangle/RectangleTest.java
@@ -0,0 +1,19 @@
+
+package com.thoughtworks.rectangle;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class RectangleTest {
+
+    @Test
+    void shouldReturnAreaWhenLengthAndBreadthArePositive() {
+        Rectangle rectangle = new Rectangle(10, 10);
+        int area = rectangle.area();
+        assertEquals(100,area);
+    }
+
+}
