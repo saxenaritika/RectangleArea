ac75d492f12afc74966e54e20980d3426da55ae6
Ritika Saxena <ritika.saxena@thoughtworks.com>
2022-09-28 16:55:17 +0530

    Refactor. hello9

--- src/test/java/com/thoughtworks/rectangle/RectangleTest.java
+++ src/test/java/com/thoughtworks/rectangle/RectangleTest.java
@@ -13,6 +13,6 @@ class RectangleTest {
     void shouldReturnAreaWhenLengthAndBreadthArePositive1() {
         Rectangle rectangle = new Rectangle(10, 10);
         int area = rectangle.area();
-assertThat(area, is(equalTo(100)));
+        assertThat(area, is(equalTo(100)));
     }
 }
