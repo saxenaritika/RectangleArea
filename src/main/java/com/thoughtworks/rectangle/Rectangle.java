d578b6468f5274d6c517ca443b9deb1656aabf35
Riya Kumari <riya.kumari@thoughtworks.com>
2022-10-07 15:43:29 +0530

    Refactor. yfhy

--- src/main/java/com/thoughtworks/rectangle/Rectangle.java
+++ src/main/java/com/thoughtworks/rectangle/Rectangle.java
@@ -1,19 +1,16 @@
 package com.thoughtworks.rectangle;
 
 public class Rectangle {
-    private final int length;
-    private final int breadth;
+    private final double length;
+    private final double breadth;
 
-
-    public Rectangle(int length, int breadth) {
+    public Rectangle(double length, double breadth) {
         this.length = length;
-
         this.breadth = breadth;
-
     }
 
-
-    public int area() {
+    public double area() {
         return this.length * this.breadth;
     }
+
 }
--- src/test/java/com/thoughtworks/rectangle/RectangleTest.java
+++ src/test/java/com/thoughtworks/rectangle/RectangleTest.java
@@ -2,19 +2,14 @@ package com.thoughtworks.rectangle;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.junit.jupiter.api.Test;
 
 class RectangleTest {
 
     @Test
-    void shouldReturnAreaWhenLengthAndBreadthArePositive() {
-
-        Rectangle rectangle = new Rectangle(10, 10);
-
-        int area = rectangle.area();
-
-        assertEquals(100,area);
+    void shouldReturnWhenLengthAndBreadthArePositive() {
+        Rectangle rectangle = new Rectangle(4.333, 2.444);
+        double area = rectangle.area();
+        assertThat(area, is(closeTo(10.58, 0.01)));
     }
 }
