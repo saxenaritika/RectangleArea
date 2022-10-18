064fd12e6f6ca6c2a5c64b3d460f8d2428ffef12
Ritika Saxena <ritika.saxena@thoughtworks.com>
2022-10-18 15:47:52 +0530

    Refactor. area

--- src/test/java/com/thoughtworks/rectangle/RectangleTest.java
+++ src/test/java/com/thoughtworks/rectangle/RectangleTest.java
@@ -9,9 +9,9 @@ import org.junit.jupiter.api.Test;
 class RectangleTest {
 
     @Test
-    void shouldReturnAreaWhenLengthAndBreadthArePositivee() {
+    void shouldReturnWhenLengthAndBreadthArePositivee() {
         Rectangle rectangle = new Rectangle(10, 10);
         int area = rectangle.area();
-        assertEquals(100,area);
+        assertThat(area,is(100));
     }
 }
